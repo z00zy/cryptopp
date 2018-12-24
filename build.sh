378bd7cbdc510bf460c8129ada2a7a98faa7a508
zhangwf <zwfmail@yahoo.com>
2018-12-24 17:26:15 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-export ANDROID_NDK_ROOT="$HOME/android/android-ndk-r16b"
+export ANDROID_NDK_ROOT="$HOME/Library/Android/android-ndk-r16b"
 export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
 
 OUTPUT="$PWD/out"
 
-for arch in arm64-v8a armeabi-v7a x86_64
-do
-    if [ "$arch" = "arm64-v8a" ]; then
-        AOSP_API_VERSION="21" source setenv-android-gcc.sh $arch
-    else
-        AOSP_API_VERSION="19" source setenv-android-gcc.sh $arch
-    fi
-
-    if [ ! -e "$OUTPUT" ]; then
-        mkdir "$OUTPUT"
-    else
+for arch in armeabi-v7a arm64-v8a; do
+    if [ -e "$OUTPUT/$arch" ]; then
         rm -rf "$OUTPUT/$arch"
     fi
+
+    AOSP_API_VERSION="21" source setenv-android-gcc.sh $arch
 
     if [ "$?" -eq "0" ]; then
         make -f GNUmakefile-cross distclean
         make -j -f GNUmakefile-cross static dynamic
         make -f GNUmakefile-cross install-lib PREFIX="$OUTPUT/$arch"
+        $STRIP --strip-debug $OUTPUT/$arch/lib/libcryptopp.a
+        $STRIP --strip-debug $OUTPUT/$arch/lib/libcryptopp.so
     fi
 done
+
